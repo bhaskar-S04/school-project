d4b14b5839028390726af3a7f7085dc70bcc20c1
bhaskar-S04 <sarenbhaskar328@gmail.com>
2025-09-04 14:18:55 +0530

    Ready for deploy

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to School Project</h1>
-      <p><a href="/addSchool">Add a School</a></p>
-      <p><a href="/showSchools">Show Schools</a></p>
+      <p>
+        <Link href="/addSchool">Add a School</Link>
+      </p>
+      <p>
+        <Link href="/showSchools">Show Schools</Link>
+      </p>
     </div>
   );
 }
